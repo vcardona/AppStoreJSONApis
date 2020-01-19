b154bfa775f8e2927a4063e47ad065d458ad339f
Victor Cardona <victorhugocardonagomez@gmail.com>
2020-01-19 10:10:13 -0500

    Initial Commit

---
+++ AppStoreJSONApis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreJSONApis
+//
+//  Created by Victor Hugo on 19/01/20.
+//  Copyright © 2020 Vintage Robot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
