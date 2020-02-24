7b6e0f23e26f9c9c5b83f0f3a3cd56f6e736eb2d
Victor Cardona <victorhugocardonagomez@gmail.com>
2020-02-24 15:41:54 -0500

    Preview Screenshot Cells

--- AppStoreJSONApis/Controller/Apps/Details/AppDetailController.swift
+++ AppStoreJSONApis/Controller/Apps/Details/AppDetailController.swift
@@ -29,38 +29,52 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
     var app: Result?
     
     let detailCellId = "detailCellId"
+    let previewCellId = "previewCellId"
     
     override func viewDidLoad(){
         super.viewDidLoad()
         collectionView.backgroundColor = .white
         
         collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: detailCellId)
+        collectionView.register(PreviewCell.self, forCellWithReuseIdentifier: previewCellId)
+        
         navigationItem.largeTitleDisplayMode = .never
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellId, for: indexPath) as! AppDetailCell
         
-        cell.app = app
-        
-        return cell
+        if indexPath.item == 0
+        {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellId, for: indexPath) as! AppDetailCell
+            cell.app = app
+            return cell
+        }else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: previewCellId, for: indexPath) as! PreviewCell
+            cell.horizontalController.app = self.app
+            return cell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
-        //calculate the necessary size for our cell somehow
+        if indexPath.item == 0{
+            //calculate the necessary size for our cell somehow
+            
+            let dummyCell = AppDetailCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000))
+            
+            dummyCell.app = app
+            dummyCell.layoutIfNeeded()
+            
+            let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
+            
+            return .init(width: view.frame.width, height: estimatedSize.height)
+        }else{
+            return .init(width: view.frame.width, height: 500)
+        }
         
-        let dummyCell = AppDetailCell(frame: .init(x: 0, y: 0, width: view.frame.width, height: 1000))
-        
-        dummyCell.app = app
-        dummyCell.layoutIfNeeded()
-        
-        let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
-        
-        return .init(width: view.frame.width, height: estimatedSize.height)
     }
 }
