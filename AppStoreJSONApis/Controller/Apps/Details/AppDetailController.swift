bdab37912604ad9d3827c803077926525ac4e0bf
Victor Cardona <victorhugocardonagomez@gmail.com>
2020-02-26 11:51:18 -0500

    App Reviews Controller

--- AppStoreJSONApis/Controller/Apps/Details/AppDetailController.swift
+++ AppStoreJSONApis/Controller/Apps/Details/AppDetailController.swift
@@ -30,6 +30,7 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
     
     let detailCellId = "detailCellId"
     let previewCellId = "previewCellId"
+    let reviewCellId = "reviewCellId"
     
     override func viewDidLoad(){
         super.viewDidLoad()
@@ -37,12 +38,13 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
         
         collectionView.register(AppDetailCell.self, forCellWithReuseIdentifier: detailCellId)
         collectionView.register(PreviewCell.self, forCellWithReuseIdentifier: previewCellId)
+        collectionView.register(ReviewRowCell.self, forCellWithReuseIdentifier: reviewCellId)
         
         navigationItem.largeTitleDisplayMode = .never
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -52,14 +54,19 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: detailCellId, for: indexPath) as! AppDetailCell
             cell.app = app
             return cell
-        }else{
+        }else if indexPath.item == 1{
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: previewCellId, for: indexPath) as! PreviewCell
             cell.horizontalController.app = self.app
+            return cell
+        }else{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reviewCellId, for: indexPath) as! ReviewRowCell
             return cell
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        var height: CGFloat = 280
         
         if indexPath.item == 0{
             //calculate the necessary size for our cell somehow
@@ -71,10 +78,15 @@ class AppDetailController: BaseListController, UICollectionViewDelegateFlowLayou
             
             let estimatedSize = dummyCell.systemLayoutSizeFitting(.init(width: view.frame.width, height: 1000))
             
-            return .init(width: view.frame.width, height: estimatedSize.height)
-        }else{
-            return .init(width: view.frame.width, height: 500)
+            height = estimatedSize.height
+        }else if indexPath.item == 1{
+            height = 500
         }
+        else{
+            height = 280
+        }
+        
+        return .init(width: view.frame.width, height: height)
         
     }
 }
---
+++ AppStoreJSONApis/Views/Apps/Details/ReviewRowCell.swift
@@ -0,0 +1,27 @@
+//
+//  ReviewRowCell.swift
+//  AppStoreJSONApis
+//
+//  Created by Victor Hugo on 26/02/20.
+//  Copyright © 2020 Vintage Robot. All rights reserved.
+//
+
+import UIKit
+
+class ReviewRowCell: UICollectionViewCell {
+    
+    let reviewsController = ReviewsController()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .yellow
+        
+        addSubview(reviewsController.view)
+        reviewsController.view.fillSuperview()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
